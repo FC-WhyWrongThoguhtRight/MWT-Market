7714bd9cbc99c5625b157bf065e1ee8e5352614e
Denny-Ko <donghar@naver.com>
2023-09-23 01:41:30 +0900

    [Feat] Security 설정
    Swagger url 접속 권한 open

--- src/main/java/org/mwt/market/config/security/SecurityConfig.java
+++ src/main/java/org/mwt/market/config/security/SecurityConfig.java
@@ -4,26 +4,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
+            "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .securityMatcher(request -> true);
 
         http
-                .cors((cors) ->
-                        cors.disable())
-                .csrf((csrf) ->
-                        csrf.disable())
+                .httpBasic(HttpBasicConfigurer::disable)
+                .formLogin(FormLoginConfigurer::disable)
+                .cors(CorsConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
         ;
 
         http
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
+                                .requestMatchers(AUTH_WHITELIST).permitAll()
                                 .anyRequest().permitAll());
 
         return http.build();
