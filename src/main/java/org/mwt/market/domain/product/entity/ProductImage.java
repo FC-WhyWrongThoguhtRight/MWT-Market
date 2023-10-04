78850f1a9eaa96434fa00f93a3ae5612a162d2ac
Denny-Ko <donghar@naver.com>
2023-10-05 00:10:28 +0900

    [Fix] 'order'가 mysql예약어라서 table을 생성하지 못하는 문제 해결

--- src/main/java/org/mwt/market/domain/product/entity/ProductImage.java
+++ src/main/java/org/mwt/market/domain/product/entity/ProductImage.java
@@ -15,10 +15,12 @@ public class ProductImage {
     private Long imgId;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
     private Product product;
 
     private String url;
 
+    @Column(name = "image_order")
     private Integer order;
 
     @CreatedDate
