d167c564893a2661062113ecc9835d95f9567f87
Denny-Ko <donghar@naver.com>
2023-10-08 03:20:27 +0900

    [Refactor] convention에 맞게 수정

--- src/main/java/org/mwt/market/domain/user/repository/UserRepository.java
+++ src/main/java/org/mwt/market/domain/user/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package org.mwt.market.domain.user.repository;
 
+import java.util.Optional;
 import org.mwt.market.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 
 @Repository
