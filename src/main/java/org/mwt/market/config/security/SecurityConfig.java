3c4a57c825a0bb818c2348f85023369cafaf9eb4
deepredk <deepred.kim@gmail.com>
2023-10-10 13:59:55 +0900

    fix: H2-Console 잘 접속되도록 함
    - h2 console 활성화
    - jpa sql 보이도록 함
    - ddl 자동 생성하도록 함
    - Spring Security 설정에서 Frame 허용함 (Same-Origin인 경우에 한하여)
      - 관련 링크: https://computer-science-student.tistory.com/497

--- src/main/java/org/mwt/market/config/security/SecurityConfig.java
+++ src/main/java/org/mwt/market/config/security/SecurityConfig.java
@@ -26,6 +26,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -116,6 +117,11 @@ public class SecurityConfig {
             .addFilterBefore(
                 new JwtAuthenticationFilter(authenticationManager(), authenticationDetailsSource()),
                 AnonymousAuthenticationFilter.class);
+
+        http
+            .headers(
+                httpSecurityHeadersConfigurer ->
+                    httpSecurityHeadersConfigurer.frameOptions(FrameOptionsConfig::sameOrigin));
 
         return http.build();
     }
