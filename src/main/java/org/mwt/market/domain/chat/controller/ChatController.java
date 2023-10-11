764780e2d3a05744ee96506778978410bb8ed9f3
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-11 11:20:23 +0900

    [refactor] develop의 채팅 관련 컨트롤러 conflict 해결

--- src/main/java/org/mwt/market/domain/chat/controller/ChatController.java
+++ src/main/java/org/mwt/market/domain/chat/controller/ChatController.java
@@ -6,8 +6,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.mwt.market.common.response.DataResponseBody;
 import org.mwt.market.config.security.token.UserPrincipal;
+import org.mwt.market.domain.chat.dto.ChatMessageDto;
 import org.mwt.market.domain.chat.dto.ChatRoomDto;
 import org.mwt.market.domain.chat.service.ChatService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,13 +58,9 @@ public class ChatController {
     @MessageMapping("/seller/chat")
     @SendTo("/chat/receive")
     @Operation(summary = "상품 판매자와 1:1 채팅 메시지 전송")
-    public ResponseEntity<ChatMessageDto> chat(
-        @RequestBody ChatMessageDto chatContent){
-        return ResponseEntity
-            .status(200)
-            .body(ChatMessageDto.builder()
-                .content("서버응답!!")
-                .build());
+    public DataResponseBody<ChatMessageDto> chat(
+        ChatMessageDto chatMessageDto){
+        return DataResponseBody.success(new ChatMessageDto("테스트"));
     }
 
 }
