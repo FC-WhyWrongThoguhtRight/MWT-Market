3be782581aa38e4148fa493352a8388cde4bdd24
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-18 11:26:55 +0900

    [refactor] joinChat swagger description 추가

--- src/main/java/org/mwt/market/domain/chat/controller/ChatController.java
+++ src/main/java/org/mwt/market/domain/chat/controller/ChatController.java
@@ -30,7 +30,7 @@ public class ChatController {
     }
 
     @PostMapping("/seller/{productId}/joinChat")
-    @Operation(summary = "상품의 채팅창에 접속")
+    @Operation(summary = "상품의 채팅창에 접속", description = "상품채팅창의 정보를 리턴합니다. 기존채팅방이 없으면 새로 생성된 채팅 방을 리턴합니다.")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200"),
         @ApiResponse(responseCode = "400")})
@@ -44,7 +44,6 @@ public class ChatController {
     @MessageMapping("/room/{roomId}")
     @SendTo("/sub/room/{roomId}")
     @Operation(summary = "상품 판매자와 1:1 채팅 메시지 전송 및 내역 저장")
-    @ApiResponses()
     public MessageResponse chat(@DestinationVariable String roomId, MessageRequest message) {
         return chatService.saveMessage(roomId, message);
     }
