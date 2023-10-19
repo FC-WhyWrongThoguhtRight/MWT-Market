19b9e2566f510250943a835d339a1f8807d0e950
Jinhong Kim <deepred.kim@gmail.com>
2023-10-19 17:54:22 +0900

    fix: 상품 등록 시 Validation 관련 추가

--- src/main/java/org/mwt/market/domain/product/dto/ProductRequestDto.java
+++ src/main/java/org/mwt/market/domain/product/dto/ProductRequestDto.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
+
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProductRequestDto {
 
     @NotBlank(message = "상품 제목은 필수 항목입니다.")
-    @Schema(description = "상품 제목", example = "test_title")
+    @Schema(description = "상품 제목", example = "test_title", maxLength = 30)
     private String title;
 
     @NotNull(message = "상품 카테고리는 필수 항목입니다.")
@@ -25,10 +27,11 @@ public class ProductRequestDto {
     private String categoryName;
 
     @NotBlank(message = "상품 내용은 필수 항목입니다.")
-    @Schema(description = "상품 설명", example = "test_content")
+    @Schema(description = "상품 설명", example = "test_content", maxLength = 3000)
     private String content;
 
     @Schema(description = "상품 가격")
+    @Pattern(regexp = "\\d+", message = "숫자 형식이어야 합니다.")
     private Integer price;
 
     @Schema(description = "상품 관련 이미지")
