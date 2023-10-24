fd4596fc0e888ba7a3f4fef0e1b63a0a322ee4fa
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-10-24 14:22:04 +0900

    [fix] 타임존 seoul 설정

--- src/main/java/org/mwt/market/MarketApplication.java
+++ src/main/java/org/mwt/market/MarketApplication.java
@@ -1,10 +1,17 @@
 package org.mwt.market;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketApplication {
+
+    @PostConstruct
+    public void timeZoneSetup() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MarketApplication.class, args);
